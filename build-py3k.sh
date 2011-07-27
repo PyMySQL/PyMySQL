82de5606755a7da2f2be2450a899ac6c4f610e2b
Pete Hunt <floydophone@gmail.com>
2011-07-27 10:55:49 -0700

    Added Py3k support

---
+++ build-py3k.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Building Python 3.0 version in ./py3k..."
+rm -fr ./py3k
+mkdir py3k
+cp -rf pymysql py3k/
+cp setup.py py3k/
+cp setup.py.py3k.patch py3k/
+cp CHANGELOG py3k/
+cp README py3k/
+cp LICENSE py3k/
+cd py3k
+2to3 .|patch -p0
+patch setup.py setup.py.py3k.patch
+cd ..
