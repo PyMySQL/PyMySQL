c35c90006b9a8b9632cad79517144108ebdb6061
Cameron Eure <cleure@websprockets.com>
2011-11-10 13:55:34 -0800

    Test case for SSCursor

--- pymysql/tests/base.py
+++ pymysql/tests/base.py
@@ -5,8 +5,7 @@ class PyMySQLTestCase(unittest.TestCase):
     # Edit this to suit your test environment.
     databases = [
         {"host":"localhost","user":"root",
-         "passwd":"","db":"test_pymysql", "use_unicode": True},
-        {"host":"localhost","user":"root","passwd":"","db":"test_pymysql2"}]
+         "passwd":"","db":"test_pymysql", "use_unicode": True}]
 
     def setUp(self):
         self.connections = []
