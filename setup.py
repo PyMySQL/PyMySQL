b82c59cac75e57a022b9762a6207223b446b0ea8
pitchum <pitchum.pourriel@free.fr>
2012-08-08 14:51:55 +0200

    Supporting both Python 2 and Python 3 with Distribute.
    http://packages.python.org/distribute/python3.html

--- setup.py
+++ setup.py
@@ -26,6 +26,10 @@ v = open(os.path.join(os.path.dirname(__file__), 'pymysql', '__init__.py'))
 version = re.compile(r".*__version__ = '(.*?)'", re.S).match(v.read()).group(1)
 v.close()
 
+extra = {}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
+
 setup(
     name = "PyMySQL",
     version = version,
@@ -38,4 +42,5 @@ setup(
     license = "MIT",
     packages = ['pymysql', 'pymysql.constants', 'pymysql.tests'],
     cmdclass = {'test': TestCommand},
+    **extra
 )
