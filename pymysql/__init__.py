16fdf9fc0be0aa4052cf7252797b1045484de6dd
Jason Madden <jason.madden@nextthought.com>
2012-12-05 08:32:05 -0600

    Use the right import

--- pymysql/__init__.py
+++ pymysql/__init__.py
@@ -92,7 +92,7 @@ def Connect(*args, **kwargs):
     from connections import Connection
     return Connection(*args, **kwargs)
     
-import pymysql.connections.Connection as _orig_conn
+from pymysql import connections as _orig_conn
 Connect.__doc__ = _orig_conn.Connection.__init__.__doc__ + """\nSee connections.Connection.__init__() for
     information about defaults."""
 del _orig_conn
