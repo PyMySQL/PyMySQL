cb9ea7545c6dbce2fe4ec5d65ffe493c307071e9
Hajime Nakagami <nakagami@gmail.com>
2012-12-30 18:26:14 +0900

    4 indent

--- pymysql/packet.py
+++ pymysql/packet.py
@@ -38,140 +38,141 @@ def unpack_uint64(n):
 
 
 class MysqlPacket(object):
-  """Representation of a MySQL response packet.  Reads in the packet
-  from the network socket, removes packet header and provides an interface
-  for reading/parsing the packet results."""
+    """Representation of a MySQL response packet.  Reads in the packet
+    from the network socket, removes packet header and provides an interface
+    for reading/parsing the packet results."""
+  
+    def __init__(self, connection):
+        self.connection = connection
+        self.__position = 0
+        self.__recv_packet()
+  
+    def __recv_from_socket(self, size):
+        r = b''
+        while size:
+            recv_data = self.connection.socket.recv(size)
+            size -= len(recv_data)
+            r += recv_data
+        return r
+  
+    def __recv_packet(self):
+        """Parse the packet header and read entire packet payload into buffer."""
+        packet_header = self.__recv_from_socket(4)
+  
+        packet_length_bin = packet_header[:3]
+        self.__packet_number = ord(packet_header[3:4])
+        # TODO: check packet_num is correct (+1 from last packet)
+        bin_length = packet_length_bin + b'\00'  # pad little-endian number
+        bytes_to_read = struct.unpack('<I', bin_length)[0]
+  
+        self.__data = self.__recv_from_socket(bytes_to_read)
+  
+    def packet_number(self): return self.__packet_number
+  
+    def get_all_data(self): return self.__data
+  
+    def read(self, size):
+        """Read the first 'size' bytes in packet and advance cursor past them."""
+        result = self.peek(size)
+        self.advance(size)
+        return result
+  
+    def read_all(self):
+        """Read all remaining data in the packet.
 
-  def __init__(self, connection):
-    self.connection = connection
-    self.__position = 0
-    self.__recv_packet()
+        (Subsequent read() or peek() will return errors.)
+        """
+        result = self.__data[self.__position:]
+        self.__position = None  # ensure no subsequent read() or peek()
+        return result
+  
+    def advance(self, length):
+        """Advance the cursor in data buffer 'length' bytes."""
+        new_position = self.__position + length
+        if new_position < 0 or new_position > len(self.__data):
+            raise Exception('Invalid advance amount (%s) for cursor.  '
+                        'Position=%s' % (length, new_position))
+        self.__position = new_position
+  
+    def rewind(self, position=0):
+        """Set the position of the data buffer cursor to 'position'."""
+        if position < 0 or position > len(self.__data):
+            raise Exception(
+                    "Invalid position to rewind cursor to: %s." % position)
+        self.__position = position
+  
+    def peek(self, size):
+        """Look at the first 'size' bytes in packet without moving cursor."""
+        result = self.__data[self.__position:(self.__position+size)]
+        if len(result) != size:
+            error = ('Result length not requested length:\n'
+                 'Expected=%s.  Actual=%s.  Position: %s.  Data Length: %s'
+                 % (size, len(result), self.__position, len(self.__data)))
+            raise AssertionError(error)
+        return result
+  
+    def get_bytes(self, position, length=1):
+        """Get 'length' bytes starting at 'position'.
+  
+        Position is start of payload (first four packet header bytes are not
+        included) starting at index '0'.
+  
+        No error checking is done.  If requesting outside end of buffer
+        an empty string (or string shorter than 'length') may be returned!
+        """
+        return self.__data[position:(position+length)]
+  
+    def read_length_coded_binary(self):
+        """Read a 'Length Coded Binary' number from the data buffer.
 
-  def __recv_from_socket(self, size):
-    r = b''
-    while size:
-      recv_data = self.connection.socket.recv(size)
-      size -= len(recv_data)
-      r += recv_data
-    return r
+        Length coded numbers can be anywhere from 1 to 9 bytes depending
+        on the value of the first byte.
+        """
+        c = ord(self.read(1))
+        if c == NULL_COLUMN:
+            return None
+        if c < UNSIGNED_CHAR_COLUMN:
+            return c
+        elif c == UNSIGNED_SHORT_COLUMN:
+            return unpack_uint16(self.read(UNSIGNED_SHORT_LENGTH))
+        elif c == UNSIGNED_INT24_COLUMN:
+            return unpack_uint24(self.read(UNSIGNED_INT24_LENGTH))
+        elif c == UNSIGNED_INT64_COLUMN:
+            # TODO: what was 'longlong'?  confirm it wasn't used?
+            return unpack_uint64(self.read(UNSIGNED_INT64_LENGTH))
+  
+    def read_length_coded_string(self):
+        """Read a 'Length Coded String' from the data buffer.
 
-  def __recv_packet(self):
-    """Parse the packet header and read entire packet payload into buffer."""
-    packet_header = self.__recv_from_socket(4)
+        A 'Length Coded String' consists first of a length coded
+        (unsigned, positive) integer represented in 1-9 bytes followed by
+        that many bytes of binary data.  (For example "cat" would be "3cat".)
+        """
+        length = self.read_length_coded_binary()
+        if length is None:
+            return None
+        return self.read(length)
+  
+    def is_ok_packet(self):
+        return ord(self.get_bytes(0)) == 0
 
-    packet_length_bin = packet_header[:3]
-    self.__packet_number = ord(packet_header[3:4])
-    # TODO: check packet_num is correct (+1 from last packet)
-    bin_length = packet_length_bin + b'\00'  # pad little-endian number
-    bytes_to_read = struct.unpack('<I', bin_length)[0]
+    def is_eof_packet(self):
+        return ord(self.get_bytes(0)) == 254  # 'fe'
 
-    self.__data = self.__recv_from_socket(bytes_to_read)
-
-  def packet_number(self): return self.__packet_number
-
-  def get_all_data(self): return self.__data
-
-  def read(self, size):
-    """Read the first 'size' bytes in packet and advance cursor past them."""
-    result = self.peek(size)
-    self.advance(size)
-    return result
-
-  def read_all(self):
-    """Read all remaining data in the packet.
-
-    (Subsequent read() or peek() will return errors.)
-    """
-    result = self.__data[self.__position:]
-    self.__position = None  # ensure no subsequent read() or peek()
-    return result
-
-  def advance(self, length):
-    """Advance the cursor in data buffer 'length' bytes."""
-    new_position = self.__position + length
-    if new_position < 0 or new_position > len(self.__data):
-      raise Exception('Invalid advance amount (%s) for cursor.  '
-                      'Position=%s' % (length, new_position))
-    self.__position = new_position
-
-  def rewind(self, position=0):
-    """Set the position of the data buffer cursor to 'position'."""
-    if position < 0 or position > len(self.__data):
-      raise Exception("Invalid position to rewind cursor to: %s." % position)
-    self.__position = position
-
-  def peek(self, size):
-    """Look at the first 'size' bytes in packet without moving cursor."""
-    result = self.__data[self.__position:(self.__position+size)]
-    if len(result) != size:
-      error = ('Result length not requested length:\n'
-               'Expected=%s.  Actual=%s.  Position: %s.  Data Length: %s'
-               % (size, len(result), self.__position, len(self.__data)))
-      raise AssertionError(error)
-    return result
-
-  def get_bytes(self, position, length=1):
-    """Get 'length' bytes starting at 'position'.
-
-    Position is start of payload (first four packet header bytes are not
-    included) starting at index '0'.
-
-    No error checking is done.  If requesting outside end of buffer
-    an empty string (or string shorter than 'length') may be returned!
-    """
-    return self.__data[position:(position+length)]
-
-  def read_length_coded_binary(self):
-    """Read a 'Length Coded Binary' number from the data buffer.
-
-    Length coded numbers can be anywhere from 1 to 9 bytes depending
-    on the value of the first byte.
-    """
-    c = ord(self.read(1))
-    if c == NULL_COLUMN:
-      return None
-    if c < UNSIGNED_CHAR_COLUMN:
-      return c
-    elif c == UNSIGNED_SHORT_COLUMN:
-      return unpack_uint16(self.read(UNSIGNED_SHORT_LENGTH))
-    elif c == UNSIGNED_INT24_COLUMN:
-      return unpack_uint24(self.read(UNSIGNED_INT24_LENGTH))
-    elif c == UNSIGNED_INT64_COLUMN:
-      # TODO: what was 'longlong'?  confirm it wasn't used?
-      return unpack_uint64(self.read(UNSIGNED_INT64_LENGTH))
-
-  def read_length_coded_string(self):
-    """Read a 'Length Coded String' from the data buffer.
-
-    A 'Length Coded String' consists first of a length coded
-    (unsigned, positive) integer represented in 1-9 bytes followed by
-    that many bytes of binary data.  (For example "cat" would be "3cat".)
-    """
-    length = self.read_length_coded_binary()
-    if length is None:
-        return None
-    return self.read(length)
-
-  def is_ok_packet(self):
-    return ord(self.get_bytes(0)) == 0
-
-  def is_eof_packet(self):
-    return ord(self.get_bytes(0)) == 254  # 'fe'
-
-  def is_resultset_packet(self):
-    field_count = ord(self.get_bytes(0))
-    return field_count >= 1 and field_count <= 250
-
-  def is_error_packet(self):
-    return ord(self.get_bytes(0)) == 255
-
-  def check_error(self):
-    if self.is_error_packet():
-      self.rewind()
-      self.advance(1)  # field_count == error (we already know that)
-      errno = unpack_uint16(self.read(2))
-      return errno, self.__data
-    return 0, None
+    def is_resultset_packet(self):
+        field_count = ord(self.get_bytes(0))
+        return field_count >= 1 and field_count <= 250
+  
+    def is_error_packet(self):
+        return ord(self.get_bytes(0)) == 255
+  
+    def check_error(self):
+        if self.is_error_packet():
+            self.rewind()
+            self.advance(1)  # field_count == error (we already know that)
+            errno = unpack_uint16(self.read(2))
+            return errno, self.__data
+        return 0, None
 
 
 class FieldDescriptorPacket(MysqlPacket):
