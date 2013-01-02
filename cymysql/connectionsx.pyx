450a9d3804cbc73535a94e4e59ba15c49c9c9932
Hajime Nakagami <nakagami@gmail.com>
2013-01-02 19:54:28 +0900

    default decoder

--- cymysql/connectionsx.pyx
+++ cymysql/connectionsx.pyx
@@ -194,7 +194,7 @@ cdef class Connection(object):
     def __init__(self, host="localhost", user=None, passwd="",
                  db=None, port=3306, unix_socket=None,
                  charset='', sql_mode=None,
-                 read_default_file=None, conv=decoders, use_unicode=None,
+                 read_default_file=None, conv=None, use_unicode=None,
                  client_flag=0, cursorclass=Cursor, init_command=None,
                  connect_timeout=None, ssl=None, read_default_group=None,
                  compress=None, named_pipe=None):
@@ -301,7 +301,7 @@ cdef class Connection(object):
         self.messages = []
         self.set_charset(charset)
         self.encoders = encoders
-        self.decoders = conv
+        self.decoders = conv if conv else decoders
 
         self._result = None
         self._affected_rows = 0
