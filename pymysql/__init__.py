be48bb67859abaf200abec66581687305c3a5de6
Marcel Rodrigues <marcelgmr@gmail.com>
2013-10-03 09:46:31 -0300

    Change brief description and update year in copyright notice.

--- pymysql/__init__.py
+++ pymysql/__init__.py
@@ -1,7 +1,7 @@
 '''
-PyMySQL: A pure-Python drop-in replacement for MySQLdb.
+PyMySQL: A pure-Python MySQL client library.
 
-Copyright (c) 2010 PyMySQL contributors
+Copyright (c) 2010, 2013 PyMySQL contributors
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
