071fa8b23e321c4b6dc37ac8702269ad44ddb46b
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2014-01-17 10:54:49 -0500

    Avoid trying to construct a docstring based on another docstring when running in "optimized" mode (when there are no docstrings).

--- pymysql/__init__.py
+++ pymysql/__init__.py
@@ -88,8 +88,10 @@ def Connect(*args, **kwargs):
     return Connection(*args, **kwargs)
 
 from pymysql import connections as _orig_conn
-Connect.__doc__ = _orig_conn.Connection.__init__.__doc__ + """\nSee connections.Connection.__init__() for
-    information about defaults."""
+if _orig_conn.Connection.__init__.__doc__ is not None:
+    Connect.__doc__ = _orig_conn.Connection.__init__.__doc__ + ("""
+See connections.Connection.__init__() for information about defaults.
+""")
 del _orig_conn
 
 def get_client_info():  # for MySQLdb compatibility
