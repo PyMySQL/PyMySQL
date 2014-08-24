2c5f000945dd56ff7836943ed5d762512804a8a2
INADA Naoki <songofacandy@gmail.com>
2014-08-25 03:16:02 +0900

    Update example.

--- example.py
+++ example.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
-
 from __future__ import print_function
 
-import pymysql
+from tornado import ioloop, gen
+import tornado_mysql
 
-conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='', db='mysql')
+@gen.coroutine
+def main():
+    conn = yield tornado_mysql.connect(host='127.0.0.1', port=3306, user='root', passwd='', db='mysql')
+    cur = conn.cursor()
+    yield cur.execute("SELECT Host,User FROM user")
+    print(cur.description)
+    print()
+    for row in cur:
+       print(row)
+    cur.close()
+    conn.close()
 
-cur = conn.cursor()
 
-cur.execute("SELECT Host,User FROM user")
-
-print(cur.description)
-
-print()
-
-for row in cur:
-   print(row)
-
-cur.close()
-conn.close()
+ioloop.IOLoop.current().run_sync(main)
