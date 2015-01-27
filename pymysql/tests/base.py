f43aeeafee313c7a9b56d6506043fcefe54fd0ca
Mike Bayer <mike_mp@zzzcomputing.com>
2015-01-27 17:50:55 -0500

    - Alter the datetime in test_basic->test_datatypes to be a fixed value without microseconds, so that no truncation or rounding of microseconds takes place; whether they are truncated or rounded depends on backend. - improve the test for test_datetime to round trip the datetime value, passing it in the INSERT as well as getting it back from the SELECT.  Use version detection to only run the test on compliant backends; don't catch and discard a ProgrammingError without knowing what it is.

--- pymysql/tests/base.py
+++ pymysql/tests/base.py
@@ -1,6 +1,7 @@
 import os
 import json
 import pymysql
+import re
 try:
     import unittest2 as unittest
 except ImportError:
@@ -18,6 +19,23 @@ class PyMySQLTestCase(unittest.TestCase):
             {"host":"localhost","user":"root",
              "passwd":"","db":"test_pymysql", "use_unicode": True, 'local_infile': True},
             {"host":"localhost","user":"root","passwd":"","db":"test_pymysql2"}]
+
+    def mysql_server_is(self, conn, version_tuple):
+        """Return True if the given connection is on the version given or
+        greater.
+
+        e.g.::
+
+            if self.mysql_server_is(conn, (5, 6, 4)):
+                # do something for MySQL 5.6.4 and above
+        """
+        server_version = conn.get_server_info()
+        server_version_tuple = tuple(
+            (int(dig) if dig is not None else 0)
+            for dig in
+            re.match(r'(\d+)\.(\d+)\.(\d+)', server_version).group(1, 2, 3)
+        )
+        return server_version_tuple >= version_tuple
 
     def setUp(self):
         self.connections = []
