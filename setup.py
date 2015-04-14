bd99b4d8575d2f49b0d5de355f588060d32fe760
INADA Naoki <songofacandy@gmail.com>
2015-04-14 23:29:39 +0900

    0.5

--- setup.py
+++ setup.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-try:
-    with open('README.rst') as f:
-        readme = f.read()
-except IOError:
-    readme = ''
-
 setup(
     name="Tornado-MySQL",
-    version="0.4",
+    version="0.5",
     url='https://github.com/PyMySQL/Tornado-MySQL',
     author='INADA Naoki',
     author_email='songofacandy@gmail.com',
     description='Pure Python MySQL Driver for Tornado',
     install_requires=['tornado>=4.0'],
-    long_description=readme,
     license="MIT",
     packages=find_packages(),
     classifiers=[
