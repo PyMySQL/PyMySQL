0635e6bdc50f6e1cbe497ceb0c2e8316fed98ed9
Hanno Schlichting <hanno@hannosch.eu>
2015-08-15 14:28:21 +0200

    Classify Geometry as a text type, refs #363.

--- pymysql/connections.py
+++ pymysql/connections.py
@@ -91,7 +91,8 @@ TEXT_TYPES = set([
     FIELD_TYPE.STRING,
     FIELD_TYPE.TINY_BLOB,
     FIELD_TYPE.VAR_STRING,
-    FIELD_TYPE.VARCHAR])
+    FIELD_TYPE.VARCHAR,
+    FIELD_TYPE.GEOMETRY])
 
 sha_new = partial(hashlib.new, 'sha1')
 
