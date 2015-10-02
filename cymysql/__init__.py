c029c9ae423e47f477ade79494032f4beb7a310b
Hajime Nakagami <nakagami@gmail.com>
2015-10-02 22:56:24 +0900

    revision number up

--- cymysql/__init__.py
+++ cymysql/__init__.py
@@ -25,7 +25,7 @@ THE SOFTWARE.
 
 import sys
 
-VERSION = (0, 8, 4)
+VERSION = (0, 8, 5)
 
 from cymysql import converters
 from cymysql.converters import escape_dict, escape_sequence, escape_string
