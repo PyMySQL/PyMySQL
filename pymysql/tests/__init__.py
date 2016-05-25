6a7cc04e95730781d269685b758087bb4c822ffa
INADA Naoki <songofacandy@gmail.com>
2016-05-25 16:46:26 +0900

    Added missing testsuite imports

--- pymysql/tests/__init__.py
+++ pymysql/tests/__init__.py
@@ -1,12 +1,15 @@
-from pymysql.tests.test_issues import *
-from pymysql.tests.test_basic import *
-from pymysql.tests.test_nextset import *
+# Sorted by alphabetical order
 from pymysql.tests.test_DictCursor import *
-from pymysql.tests.test_connection import *
 from pymysql.tests.test_SSCursor import *
-from pymysql.tests.test_load_local import *
-from pymysql.tests.test_optionfile import *
+from pymysql.tests.test_basic import *
+from pymysql.tests.test_connection import *
 from pymysql.tests.test_converters import *
+from pymysql.tests.test_cursor import *
+from pymysql.tests.test_example import *
+from pymysql.tests.test_issues import *
+from pymysql.tests.test_load_local import *
+from pymysql.tests.test_nextset import *
+from pymysql.tests.test_optionfile import *
 
 from pymysql.tests.thirdparty import *
 
