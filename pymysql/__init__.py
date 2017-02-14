34de70f120bd6ec99984fe4821b5485014b33c30
INADA Naoki <songofacandy@gmail.com>
2017-02-14 21:33:00 +0900

    0.7.10

--- pymysql/__init__.py
+++ pymysql/__init__.py
@@ -35,7 +35,7 @@ from .times import (
     DateFromTicks, TimeFromTicks, TimestampFromTicks)
 
 
-VERSION = (0, 7, 9, None)
+VERSION = (0, 7, 10, None)
 threadsafety = 1
 apilevel = "2.0"
 paramstyle = "pyformat"
