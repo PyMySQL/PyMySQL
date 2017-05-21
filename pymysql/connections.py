e3d2006c3815bce614407fee02eeead4a402b1b0
Spencer Mitchell <smitchell556@gmail.com>
2017-05-21 11:44:46 -0700

    update Connection docstring to use sphinx style params

--- pymysql/connections.py
+++ pymysql/connections.py
@@ -524,50 +524,50 @@ class Connection(object):
     Establish a connection to the MySQL database. Accepts several
     arguments:
 
-    host: Host where the database server is located
-    user: Username to log in as
-    password: Password to use.
-    database: Database to use, None to not use a particular one.
-    port: MySQL port to use, default is usually OK. (default: 3306)
-    bind_address: When the client has multiple network interfaces, specify
+    :param host: Host where the database server is located
+    :param user: Username to log in as
+    :param password: Password to use.
+    :param database: Database to use, None to not use a particular one.
+    :param port: MySQL port to use, default is usually OK. (default: 3306)
+    :param bind_address: When the client has multiple network interfaces, specify
         the interface from which to connect to the host. Argument can be
         a hostname or an IP address.
-    unix_socket: Optionally, you can use a unix socket rather than TCP/IP.
-    charset: Charset you want to use.
-    sql_mode: Default SQL_MODE to use.
-    read_default_file:
+    :param unix_socket: Optionally, you can use a unix socket rather than TCP/IP.
+    :param charset: Charset you want to use.
+    :param sql_mode: Default SQL_MODE to use.
+    :param read_default_file:
         Specifies  my.cnf file to read these parameters from under the [client] section.
-    conv:
+    :param conv:
         Conversion dictionary to use instead of the default one.
         This is used to provide custom marshalling and unmarshaling of types.
         See converters.
-    use_unicode:
+    :param use_unicode:
         Whether or not to default to unicode strings.
         This option defaults to true for Py3k.
-    client_flag: Custom flags to send to MySQL. Find potential values in constants.CLIENT.
-    cursorclass: Custom cursor class to use.
-    init_command: Initial SQL statement to run when connection is established.
-    connect_timeout: Timeout before throwing an exception when connecting.
+    :param client_flag: Custom flags to send to MySQL. Find potential values in constants.CLIENT.
+    :param cursorclass: Custom cursor class to use.
+    :param init_command: Initial SQL statement to run when connection is established.
+    :param connect_timeout: Timeout before throwing an exception when connecting.
         (default: 10, min: 1, max: 31536000)
-    ssl:
+    :param ssl:
         A dict of arguments similar to mysql_ssl_set()'s parameters.
         For now the capath and cipher arguments are not supported.
-    read_default_group: Group to read from in the configuration file.
-    compress; Not supported
-    named_pipe: Not supported
-    autocommit: Autocommit mode. None means use server default. (default: False)
-    local_infile: Boolean to enable the use of LOAD DATA LOCAL command. (default: False)
-    max_allowed_packet: Max size of packet sent to server in bytes. (default: 16MB)
+    :param read_default_group: Group to read from in the configuration file.
+    :param compress: Not supported
+    :param named_pipe: Not supported
+    :param autocommit: Autocommit mode. None means use server default. (default: False)
+    :param local_infile: Boolean to enable the use of LOAD DATA LOCAL command. (default: False)
+    :param max_allowed_packet: Max size of packet sent to server in bytes. (default: 16MB)
         Only used to limit size of "LOAD LOCAL INFILE" data packet smaller than default (16KB).
-    defer_connect: Don't explicitly connect on contruction - wait for connect call.
+    :param defer_connect: Don't explicitly connect on contruction - wait for connect call.
         (default: False)
-    auth_plugin_map: A dict of plugin names to a class that processes that plugin.
+    :param auth_plugin_map: A dict of plugin names to a class that processes that plugin.
         The class will take the Connection object as the argument to the constructor.
         The class needs an authenticate method taking an authentication packet as
         an argument.  For the dialog plugin, a prompt(echo, prompt) method can be used
         (if no authenticate method) for returning a string from the user. (experimental)
-    db: Alias for database. (for compatibility to MySQLdb)
-    passwd: Alias for password. (for compatibility to MySQLdb)
+    :param db: Alias for database. (for compatibility to MySQLdb)
+    :param passwd: Alias for password. (for compatibility to MySQLdb)
     """
 
     _sock = None
