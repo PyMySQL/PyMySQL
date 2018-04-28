ee6c23321589db6d963cffd5cbfba2a9857013ec
Daniel Black <grooverdan@users.sourceforge.net>
2018-04-28 18:24:09 +1000

    travis: mysql_native_password for mysql:8.0

--- .travis/initializedb.sh
+++ .travis/initializedb.sh
@@ -30,8 +30,13 @@ if [ ! -z "${DB}" ]; then
     mysql -uroot -e 'create database test1 DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;'
     mysql -uroot -e 'create database test2 DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;'
 
-    mysql -u root -e "create user test2           identified by 'some password'; grant all on test2.* to test2;"
-    mysql -u root -e "create user test2@localhost identified by 'some password'; grant all on test2.* to test2@localhost;"
+    if [ $DB == 'mysql:8.0' ]; then
+        WITH_PLUGIN='with mysql_native_password'
+    else
+        WITH_PLUGIN=''
+    fi
+    mysql -u root -e "create user test2           identified ${WITH_PLUGIN} by 'some password'; grant all on test2.* to test2;"
+    mysql -u root -e "create user test2@localhost identified ${WITH_PLUGIN} by 'some password'; grant all on test2.* to test2@localhost;"
 
     cp .travis/docker.json pymysql/tests/databases.json
 else
