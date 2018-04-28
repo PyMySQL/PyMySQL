13d08a98d7f42c876dcfcfe364a3a65a9341049b
Daniel Black <grooverdan@users.sourceforge.net>
2018-04-28 13:50:47 +1000

    travis: test using docker MySQL-5.5 and 5.6

--- .travis/initializedb.sh
+++ .travis/initializedb.sh
@@ -9,8 +9,8 @@ if [ ! -z "${DB}" ]; then
     # disable existing database server in case of accidential connection
     sudo service mysql stop
 
-    docker pull mysql:${DB}
-    docker run -it --name=mysqld -d -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -p 3306:3306 mysql:${DB}
+    docker pull ${DB}
+    docker run -it --name=mysqld -d -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -p 3306:3306 ${DB}
     sleep 10
 
     while :
