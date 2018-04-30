ccef546213b0d9e994af28c0a8ef2e579cde5831
Daniel Black <danielgb@au.ibm.com>
2018-04-30 16:40:04 +1000

    SSCursor: failed to clear unused results on deletion (#655)
    A cursor may contain unretrieved results. These should be consumed
    before a new cursor is created on the same connection.

--- pymysql/cursors.py
+++ pymysql/cursors.py
@@ -446,6 +446,8 @@ class SSCursor(Cursor):
         finally:
             self.connection = None
 
+    __del__ = close
+
     def _query(self, q):
         conn = self._get_db()
         self._last_executed = q
