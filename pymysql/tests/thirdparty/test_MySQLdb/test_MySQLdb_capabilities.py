c27e443ab882d9716f0e9fab8d75484791d199c1
Daniel Black <daniel@linux.ibm.com>
2018-05-01 21:01:31 +1000

    test_MySQLdb_capabilities: create_table_extra from parent class. + charset=utf8mb4

--- pymysql/tests/thirdparty/test_MySQLdb/test_MySQLdb_capabilities.py
+++ pymysql/tests/thirdparty/test_MySQLdb/test_MySQLdb_capabilities.py
@@ -16,9 +16,8 @@ class test_MySQLdb(capabilities.DatabaseTest):
     connect_kwargs = base.PyMySQLTestCase.databases[0].copy()
     connect_kwargs.update(dict(read_default_file='~/.my.cnf',
                           use_unicode=True, binary_prefix=True,
-                          charset='utf8', sql_mode="ANSI,STRICT_TRANS_TABLES,TRADITIONAL"))
+                          charset='utf8mb4', sql_mode="ANSI,STRICT_TRANS_TABLES,TRADITIONAL"))
 
-    create_table_extra = "ENGINE=INNODB CHARACTER SET UTF8"
     leak_test = False
 
     def quote_identifier(self, ident):
