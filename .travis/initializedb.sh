49a64f3ffd321627694139dfdd4ae0999a8b9a1a
Daniel Black <daniel@linux.ibm.com>
2018-05-07 18:58:28 +1000

    travis: Add MySQL 8.0 (#663)

--- .travis/initializedb.sh
+++ .travis/initializedb.sh
@@ -13,10 +13,13 @@ if [ ! -z "${DB}" ]; then
     docker run -it --name=mysqld -d -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -p 3306:3306 ${DB}
     sleep 10
 
+    mysql() {
+        docker exec mysqld mysql "${@}"
+    }
     while :
     do
         sleep 5
-        mysql -uroot -h 127.0.0.1 -P 3306 -e 'select version()'
+        mysql -e 'select version()'
         if [ $? = 0 ]; then
             break
         fi
@@ -24,14 +27,25 @@ if [ ! -z "${DB}" ]; then
         docker logs --tail 5 mysqld
     done
 
-    echo -e "[client]\nhost = 127.0.0.1\n" > "${HOME}"/.my.cnf
-
     mysql -e 'select VERSION()'
-    mysql -uroot -e 'create database test1 DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;'
-    mysql -uroot -e 'create database test2 DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;'
 
-    mysql -u root -e "create user test2           identified by 'some password'; grant all on test2.* to test2;"
-    mysql -u root -e "create user test2@localhost identified by 'some password'; grant all on test2.* to test2@localhost;"
+    if [ $DB == 'mysql:8.0' ]; then
+        WITH_PLUGIN='with mysql_native_password'
+        mysql -e 'SET GLOBAL local_infile=on'
+        docker cp mysqld:/var/lib/mysql/public_key.pem "${HOME}"
+        docker cp mysqld:/var/lib/mysql/ca.pem "${HOME}"
+        docker cp mysqld:/var/lib/mysql/server-cert.pem "${HOME}"
+        docker cp mysqld:/var/lib/mysql/client-key.pem "${HOME}"
+        docker cp mysqld:/var/lib/mysql/client-cert.pem "${HOME}"
+    else
+        WITH_PLUGIN=''
+    fi
+
+    mysql -uroot -e 'create database test1 DEFAULT CHARACTER SET utf8mb4'
+    mysql -uroot -e 'create database test2 DEFAULT CHARACTER SET utf8mb4'
+
+    mysql -u root -e "create user test2           identified ${WITH_PLUGIN} by 'some password'; grant all on test2.* to test2;"
+    mysql -u root -e "create user test2@localhost identified ${WITH_PLUGIN} by 'some password'; grant all on test2.* to test2@localhost;"
 
     cp .travis/docker.json pymysql/tests/databases.json
 else
