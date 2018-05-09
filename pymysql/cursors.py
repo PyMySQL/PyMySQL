9105a9ebc98e280a3a4731bec928277714ae6a93
INADA Naoki <songofacandy@gmail.com>
2018-05-09 19:59:36 +0900

    Remove unused variable

--- pymysql/cursors.py
+++ pymysql/cursors.py
@@ -331,7 +331,7 @@ class Cursor(object):
 
     def _clear_result(self):
         self.rownumber = 0
-        self._result = result = None
+        self._result = None
 
         self.rowcount = 0
         self.description = None
