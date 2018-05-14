2287d13698d818b57d9c356ce097a422a348dfa7
Hajime Nakagami <nakagami@gmail.com>
2018-05-14 17:09:45 +0900

    revision number up

--- cymysql/__init__.py
+++ cymysql/__init__.py
@@ -32,7 +32,7 @@ from cymysql.times import Date, Time, Timestamp, \
 from cymysql.connections import Connection
 from cymysql.constants import FIELD_TYPE
 
-VERSION = (0, 9, 7)
+VERSION = (0, 9, 8)
 threadsafety = 1
 apilevel = "2.0"
 paramstyle = "format"
