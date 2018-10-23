7e41330ba0f5455e61f782514d843f884854a021
Inada Naoki <songofacandy@gmail.com>
2018-10-23 09:09:05 +0900

    Charsets.by_name() return None when no default collation

--- pymysql/charset.py
+++ pymysql/charset.py
@@ -35,13 +35,13 @@ class Charsets:
     def add(self, c):
         self._by_id[c.id] = c
         if c.is_default:
-            self._by_name[c.name]
+            self._by_name[c.name] = c
 
     def by_id(self, id):
         return self._by_id[id]
 
     def by_name(self, name):
-        return self._by_name[name.lower()]
+        return self._by_name.get(name.lower())
 
 _charsets = Charsets()
 """
