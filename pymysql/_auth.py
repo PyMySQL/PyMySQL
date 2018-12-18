4bf04205359251ca35015a01359d6687d0b4bbcf
INADA Naoki <methane@users.noreply.github.com>
2018-12-18 19:25:12 +0900

    Optional cryptography (#760)

--- pymysql/_auth.py
+++ pymysql/_auth.py
@@ -7,9 +7,13 @@ from .err import OperationalError
 from .util import byte2int, int2byte
 
 
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization, hashes
-from cryptography.hazmat.primitives.asymmetric import padding
+try:
+    from cryptography.hazmat.backends import default_backend
+    from cryptography.hazmat.primitives import serialization, hashes
+    from cryptography.hazmat.primitives.asymmetric import padding
+    _have_cryptography = True
+except ImportError:
+    _have_cryptography = False
 
 from functools import partial
 import hashlib
@@ -134,6 +138,8 @@ def sha2_rsa_encrypt(password, salt, public_key):
 
     Used for sha256_password and caching_sha2_password.
     """
+    if not _have_cryptography:
+        raise RuntimeError("cryptography is required for sha256_password or caching_sha2_password")
     message = _xor_password(password + b'\0', salt)
     rsa_key = serialization.load_pem_public_key(public_key, default_backend())
     return rsa_key.encrypt(
