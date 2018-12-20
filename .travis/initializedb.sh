60b6dfe9a3c844aa666e54d9486f2c9af72c6362
Inada Naoki <songofacandy@gmail.com>
2018-12-20 14:21:27 +0900

    fix travis

--- .travis/initializedb.sh
+++ .travis/initializedb.sh
@@ -4,12 +4,7 @@
 set -exv
 
 if [ ! -z "${DB}" ]; then
-    # disable existing database server in case of accidential connection
-    sudo service mysql stop
-
-    docker pull ${DB}
     docker run -it --name=mysqld -d -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -p 3306:3306 ${DB}
-    sleep 10
 
     mysql() {
         docker exec mysqld mysql "${@}"
@@ -17,15 +12,10 @@ if [ ! -z "${DB}" ]; then
     while :
     do
         sleep 5
-        mysql -e 'select version()'
-        if [ $? = 0 ]; then
-            break
-        fi
+        mysql -e 'select version()' && break
         echo "server logs"
         docker logs --tail 5 mysqld
     done
-
-    mysql -e 'select VERSION()'
 
     if [ $DB == 'mysql:8.0' ]; then
         WITH_PLUGIN='with mysql_native_password'
