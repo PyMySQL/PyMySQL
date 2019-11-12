4455ca8379cf80312b0ceca8cdcd48a4e4fd74df
Inada Naoki <songofacandy@gmail.com>
2019-11-12 21:52:42 +0900

    Use OperationalError for unknown error with code>1000.
    Fixes #816.

--- pymysql/err.py
+++ pymysql/err.py
@@ -100,5 +100,7 @@ del _map_error, ER
 def raise_mysql_exception(data):
     errno = struct.unpack('<h', data[1:3])[0]
     errval = data[9:].decode('utf-8', 'replace')
-    errorclass = error_map.get(errno, InternalError)
+    errorclass = error_map.get(errno)
+    if errorclass is None:
+        errorclass = InternalError if errorno < 1000 else OperationalError
     raise errorclass(errno, errval)
