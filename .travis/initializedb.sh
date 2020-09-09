7ac15f5d850552f3a53d3f020622172574b8e267
Inada Naoki <songofacandy@gmail.com>
2020-09-09 17:34:15 +0900

    sha2 auth: Use 20+ length password

--- .travis/initializedb.sh
+++ .travis/initializedb.sh
@@ -27,9 +27,9 @@ if [ $DB == 'mysql:8.0' ]; then
     # Test user for auth test
     mysql -e '
         CREATE USER
-            user_sha256   IDENTIFIED WITH "sha256_password" BY "pass_sha256",
+            user_sha256   IDENTIFIED WITH "sha256_password" BY "pass_sha256_01234567890123456789",
             nopass_sha256 IDENTIFIED WITH "sha256_password",
-            user_caching_sha2   IDENTIFIED WITH "caching_sha2_password" BY "pass_caching_sha2",
+            user_caching_sha2   IDENTIFIED WITH "caching_sha2_password" BY "pass_caching_sha2_01234567890123456789",
             nopass_caching_sha2 IDENTIFIED WITH "caching_sha2_password"
             PASSWORD EXPIRE NEVER;'
     mysql -e 'GRANT RELOAD ON *.* TO user_caching_sha2;'
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -12,6 +12,9 @@ port = 3306
 ca = os.path.expanduser("~/ca.pem")
 ssl = {'ca': ca, 'check_hostname': False}
 
+pass_sha256 = "pass_sha256_01234567890123456789"
+pass_caching_sha2 = "pass_caching_sha2_01234567890123456789"
+
 
 def test_sha256_no_password():
     con = pymysql.connect(user="nopass_sha256", host=host, port=port, ssl=None)
@@ -24,12 +27,12 @@ def test_sha256_no_passowrd_ssl():
 
 
 def test_sha256_password():
-    con = pymysql.connect(user="user_sha256", password="pass_sha256", host=host, port=port, ssl=None)
+    con = pymysql.connect(user="user_sha256", password=pass_sha256, host=host, port=port, ssl=None)
     con.close()
 
 
 def test_sha256_password_ssl():
-    con = pymysql.connect(user="user_sha256", password="pass_sha256", host=host, port=port, ssl=ssl)
+    con = pymysql.connect(user="user_sha256", password=pass_sha256, host=host, port=port, ssl=ssl)
     con.close()
 
 
@@ -44,20 +47,20 @@ def test_caching_sha2_no_password():
 
 
 def test_caching_sha2_password():
-    con = pymysql.connect(user="user_caching_sha2", password="pass_caching_sha2", host=host, port=port, ssl=None)
+    con = pymysql.connect(user="user_caching_sha2", password=pass_caching_sha2, host=host, port=port, ssl=None)
     con.close()
 
     # Fast path of caching sha2
-    con = pymysql.connect(user="user_caching_sha2", password="pass_caching_sha2", host=host, port=port, ssl=None)
+    con = pymysql.connect(user="user_caching_sha2", password=pass_caching_sha2, host=host, port=port, ssl=None)
     con.query("FLUSH PRIVILEGES")
     con.close()
 
 
 def test_caching_sha2_password_ssl():
-    con = pymysql.connect(user="user_caching_sha2", password="pass_caching_sha2", host=host, port=port, ssl=ssl)
+    con = pymysql.connect(user="user_caching_sha2", password=pass_caching_sha2, host=host, port=port, ssl=ssl)
     con.close()
 
     # Fast path of caching sha2
-    con = pymysql.connect(user="user_caching_sha2", password="pass_caching_sha2", host=host, port=port, ssl=None)
+    con = pymysql.connect(user="user_caching_sha2", password=pass_caching_sha2, host=host, port=port, ssl=None)
     con.query("FLUSH PRIVILEGES")
     con.close()
