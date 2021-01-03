df14c55377867b7a5a159a3ed5f0280b1cf10aea
Inada Naoki <songofacandy@gmail.com>
2021-01-03 12:23:22 +0900

    black setup.py

--- setup.py
+++ setup.py
@@ -4,38 +4,38 @@ from setuptools import setup, find_packages
 
 version = "0.10.1"
 
-with io.open('./README.rst', encoding='utf-8') as f:
+with io.open("./README.rst", encoding="utf-8") as f:
     readme = f.read()
 
 setup(
     name="PyMySQL",
     version=version,
-    url='https://github.com/PyMySQL/PyMySQL/',
+    url="https://github.com/PyMySQL/PyMySQL/",
     project_urls={
         "Documentation": "https://pymysql.readthedocs.io/",
     },
-    description='Pure Python MySQL Driver',
+    description="Pure Python MySQL Driver",
     long_description=readme,
-    packages=find_packages(exclude=['tests*', 'pymysql.tests*']),
+    packages=find_packages(exclude=["tests*", "pymysql.tests*"]),
     extras_require={
         "rsa": ["cryptography"],
         "ed25519": ["PyNaCl>=1.4.0"],
     },
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Topic :: Database',
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Database",
     ],
     keywords="MySQL",
 )
