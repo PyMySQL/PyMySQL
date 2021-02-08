0d7abbd528af66c6905134a024a1ab0d0c98f4e5
Hajime Nakagami <nakagami@gmail.com>
2021-02-08 13:26:48 +0900

    remove thirdparty tests (No longer does not works on Py3)

--- cymysql/tests/__init__.py
+++ cymysql/tests/__init__.py
@@ -3,11 +3,6 @@ from cymysql.tests.test_example import *
 from cymysql.tests.test_basic import *
 from cymysql.tests.test_DictCursor import *
 
-import sys
-if sys.version_info[0] == 2:
-    # MySQLdb tests were designed for Python 3
-    from cymysql.tests.thirdparty import *
-
 if __name__ == "__main__":
     import unittest
     unittest.main()
--- cymysql/tests/thirdparty/__init__.py
+++
@@ -1,5 +0,0 @@
-from test_MySQLdb import *
-
-if __name__ == "__main__":
-    import unittest
-    unittest.main()
