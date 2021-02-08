3a8a2a5397b44b5efb9a053150854d2fe80e7117
Hajime Nakagami <nakagami@gmail.com>
2021-02-08 13:35:51 +0900

    fix flake8 error

--- misc/mysqlproxy.py
+++ misc/mysqlproxy.py
@@ -22,6 +22,7 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 ##############################################################################
+from __future__ import print_function
 import sys
 import time
 import socket
