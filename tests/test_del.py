caa3c271b6d42e032664b9f26631b62c6cab58fc
Roel Schroeven <roel@tresco.eu>
2021-02-12 17:27:07 +0100

    Tests for close() and __del__()

---
+++ tests/test_del.py
@@ -0,0 +1,29 @@
+"""Test close() and __del__()"""
+
+import os
+import pymysql
+
+# pymysql.connections.DEBUG = True
+# pymysql._auth.DEBUG = True
+
+host = "127.0.0.1"
+port = 3306
+
+def test_close():
+    con = pymysql.connect(user="nopass_sha256", host=host, port=port, ssl=None)
+    con.close()
+
+def test_del():
+    con = pymysql.connect(user="nopass_sha256", host=host, port=port, ssl=None)
+    del con
+
+def test_close_and_explicit_del():
+    con = pymysql.connect(user="nopass_sha256", host=host, port=port, ssl=None)
+    con.close()
+    # Call __del__() explicitly as a test (don't do this in normal code)
+    con.__del__()
+
+def test_explicit_del():
+    con = pymysql.connect(user="nopass_sha256", host=host, port=port, ssl=None)
+    # Call __del__() explicitly as a test (don't do this in normal code)
+    con.__del__()
