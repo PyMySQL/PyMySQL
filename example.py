60a0c25e3b4a81742f591990d8e59b72625e6b63
Hajime Nakagami <nakagami@gmail.com>
2021-02-19 16:07:43 +0900

    rganized example code

--- example.py
+++ example.py
@@ -2,20 +2,14 @@
 
 import cymysql
 
-#conn = cymysql.connect(host='127.0.0.1', unix_socket='/tmp/mysql.sock', user='root', passwd=None, db='mysql')
-
 conn = cymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='', db='mysql')
-   
 
 cur = conn.cursor()
 
 cur.execute("SELECT Host,User FROM user")
 
-# print cur.description
+print(cur.description)
 
-# r = cur.fetchall()
-# print r
-# ...or...
 for r in cur.fetchall():
    print(r)
 
