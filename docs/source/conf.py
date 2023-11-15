f62893a2c284468091efc95e5b744abcf274dc34
Inada Naoki <songofacandy@gmail.com>
2023-11-15 13:57:25 +0900

    update document settings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ sys.path.insert(0, os.path.abspath("../../"))
 # ones.
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,7 +47,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "PyMySQL"
-copyright = "2016, Yutaka Matsubara and GitHub contributors"
+copyright = "2023, Inada Naoki and GitHub contributors"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -103,6 +103,7 @@ pygments_style = "sphinx"
 # a list of builtin themes.
 # html_theme = "default"
 html_theme = "sphinx_rtd_theme"
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
