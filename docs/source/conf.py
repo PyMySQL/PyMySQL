eb0b6e3429fcd1971be56cae32ffe5780c1c9cb6
Inada Naoki <songofacandy@gmail.com>
2023-11-15 13:57:25 +0900

    update document settings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,6 @@ sys.path.insert(0, os.path.abspath("../../"))
 # ones.
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -101,9 +100,7 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-# html_theme = "default"
-html_theme = "sphinx_rtd_theme"
-
+ html_theme = "default"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
