653cd3d8ee8a560588378ec8481ca26e92615b35
Hajime Nakagami <nakagami@gmail.com>
2024-08-17 12:43:40 +0900

    add SocketWrapper and AsyncSocketWrapper

---
+++ cymysql/aio/socketwrapper.py
@@ -0,0 +1,29 @@
+from ..socketwrapper import SocketWrapper
+from ..err import OperationalError
+from ..packet import unpack_uint24
+
+
+class AsyncSocketWrapper(SocketWrapper):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    async def recv(self, size, loop):
+        r = b''
+        while size:
+            recv_data = await loop.sock_recv(self._sock, size)
+            if not recv_data:
+                raise OperationalError(2013, "Lost connection to MySQL server during query")
+            size -= len(recv_data)
+            r += recv_data
+        return r
+
+    async def recv_packet(self, loop):
+        """Read entire mysql packet."""
+        recv_data = b''
+        while True:
+            ln = unpack_uint24(await self.recv(4, loop))
+            recv_data += await self.recv(ln, loop)
+            if recv_data[:3] != b'\xff\xff\xff':
+                break
+
+        return recv_data
---
+++ cymysql/socketwarapper.py
@@ -0,0 +1,29 @@
+from cymysql.err import OperationalError
+from cymysql.packet import unpack_uint24
+
+
+class SocketWrapper():
+    def __init__(self, sock):
+        self._sock = sock
+
+    def recv(self, size):
+        r = b''
+        while size:
+            recv_data = self._sock.recv(size)
+            if not recv_data:
+                raise OperationalError(2013, "Lost connection to MySQL server during query")
+            size -= len(recv_data)
+            r += recv_data
+        return r
+
+    def recv_packet(self):
+        """Read entire mysql packet."""
+        recv_data = b''
+        while True:
+            recv_data += self.recv(unpack_uint24(self.recv(4)))
+            if recv_data[:3] != b'\xff\xff\xff':
+                break
+        return recv_data
+
+    def sendall(self, data):
+        self._sock.sendall(data)
