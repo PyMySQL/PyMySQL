03a0d561929074f0de83d6200763efcb1a2e9e2d
Hajime Nakagami <nakagami@gmail.com>
2024-08-19 15:23:58 +0900

    socket wrapper compress ready

--- cymysql/aio/socketwrapper.py
+++ cymysql/aio/socketwrapper.py
@@ -1,4 +1,5 @@
-from ..socketwrapper import SocketWrapper
+import zlib
+from ..socketwrapper import SocketWrapper, pack_int24, unpack_uint24
 from ..err import OperationalError
 
 
@@ -16,16 +17,41 @@ class AsyncSocketWrapper(SocketWrapper):
             r += recv_data
         return r
 
+    async def recv_uncompress_packet(self):
+        return await self.recv(unpack_uint24(await self.recv(4)))
+
+    async def _recv_from_decompressed(self, size):
+        if len(self._decompressed) < size:
+            compressed_length = unpack_uint24(await self.recv(3))
+            await self.recv(1)  # compressed sequence
+            uncompressed_length = unpack_uint24(await self.recv(3))
+            data = await self.recv(compressed_length)
+            if uncompressed_length != 0:
+                data = zlib.decompress(data)
+                assert len(data) == uncompressed_length
+            self._decompressed += data
+        recv_data, self._decompressed = self._decompressed[:size], self._decompressed[size:]
+        return recv_data
+
     async def recv_packet(self, loop):
         """Read entire mysql packet."""
         recv_data = b''
-        while True:
-            ln = int.from_bytes((await self.recv(4, loop))[:3], "little")
-            recv_data += await self.recv(ln, loop)
-            if recv_data[:3] != b'\xff\xff\xff':
-                break
+        if self._compress:
+            ln = unpack_uint24(await self._recv_from_decompressed(4))
+            recv_data = await self._recv_from_decompressed(ln)
+        else:
+            while True:
+                ln = int.from_bytes((await self.recv(4, loop))[:3], "little")
+                recv_data += await self.recv(ln, loop)
+                if recv_data[:3] != b'\xff\xff\xff':
+                    break
 
         return recv_data
 
+    async def send_uncompress_packet(self, data, loop):
+        await loop.sock_sendall(self._sock, data)
+
     async def send_packet(self, data, loop):
+        if self._compress:
+            data = pack_int24(len(data)) + b'\x00' + pack_int24(0) + data
         await loop.sock_sendall(self._sock, data)
--- cymysql/socketwrapper.pyx
+++ cymysql/socketwrapper.pyx
@@ -1,6 +1,11 @@
 import sys
+import zlip
 from cymysql.err import OperationalError
 from libc.stdint cimport uint16_t, uint32_t
+
+
+cdef bytes pack_int24(int n):
+    return bytes([n & 0xFF, (n >> 8) & 0xFF, (n >> 16) & 0xFF])
 
 
 cdef uint32_t unpack_uint24(bytes s):
@@ -11,10 +16,12 @@ cdef uint32_t unpack_uint24(bytes s):
 cdef class SocketWrapper():
     cdef public object _sock
     cdef public bool _compress
+    cdef public bool _decompressed
 
     def __init__(self, sock, compress):
         self._sock = sock
         self._compress = compress
+        self._decompressed = b''
 
     cdef recv(self, size):
         r = b''
@@ -26,16 +33,41 @@ cdef class SocketWrapper():
             r += recv_data
         return r
 
+    def recv_uncompress_packet(self):
+        return self.recv(unpack_uint24(self.recv(4)))
+
+    cdef bytes _recv_from_decompressed(self, size):
+        if len(self._decompressed) < size:
+            compressed_length = unpack_uint24(self.recv(3))
+            self.recv(1)  # compressed sequence
+            uncompressed_length = unpack_uint24(self.recv(3))
+            data = self.recv(compressed_length)
+            if uncompressed_length != 0:
+                data = zlib.decompress(data)
+                assert len(data) == uncompressed_length
+            self._decompressed += data
+        recv_data, self._decompressed = self._decompressed[:size], self._decompressed[size:]
+        return recv_data
+
     def recv_packet(self):
         """Read entire mysql packet."""
         recv_data = b''
-        while True:
-            recv_data += self.recv(unpack_uint24(self.recv(4)))
-            if recv_data[:3] != b'\xff\xff\xff':
-                break
+        if self._compress:
+            ln = unpack_uint24(self._recv_from_decompressed(4))
+            recv_data = self._recv_from_decompressed(ln)
+        else:
+            while True:
+                recv_data += self.recv(unpack_uint24(self.recv(4)))
+                if recv_data[:3] != b'\xff\xff\xff':
+                    break
         return recv_data
 
+    def send_uncompress_packet(self, data):
+        self._sock.sendall(data)
+
     def send_packet(self, data):
+        if self._compress:
+            data = pack_int24(len(data)) + b'\x00' + pack_int24(0) + data
         self._sock.sendall(data)
 
     def setblocking(self, b):
