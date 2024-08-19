0dd09f3caec2875f2f3eac49a9aec974346c542e
Hajime Nakagami <nakagami@gmail.com>
2024-08-19 16:12:00 +0900

    tests with compress and uncompress protocol

--- cymysql/tests/base.py
+++ cymysql/tests/base.py
@@ -8,9 +8,9 @@ class PyMySQLTestCase(unittest.TestCase):
     test_host = "127.0.0.1"
     test_passwd = os.environ.get("MYSQL_ROOT_PASSWORD", "")
     databases = [
-        {"host": test_host, "user": "root",
-         "passwd": test_passwd, "db": "test_cymysql", "use_unicode": True},
-        {"host": test_host, "user": "root", "passwd": test_passwd, "db": "test_cymysql2"}]
+        {"host": test_host, "user": "root", "passwd": test_passwd, "db": "test_cymysql", "compress": True},
+        {"host": test_host, "user": "root", "passwd": test_passwd, "db": "test_cymysql2", "compress": False},
+    ]
 
     def setUp(self):
         self.connections = []
